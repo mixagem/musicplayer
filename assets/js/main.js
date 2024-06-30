5e6c82650b32902bf75b4caf8a43c39551188324
Filipe Teixeira <filipeteixeira141@gmail.com>
2024-06-30 13:11:17 +0100

    v4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,14 @@
 /**
   * Mixagem audio player
-  * version 2.0 - 12.12.2021 
+  * version 3.0 - 13.12.2021 
   * https://mambosinfinitos.pt
   */
 
 function mixagemAudioPlayer() {
+
+  const megaArray = [['1. Wavy The Pirate - Loka', '2. CREWELLA - Ainda é Longe ft. Ferry e Vando Streets'], ['2:43', '3:02']];
+  const trackCount = megaArray[0].length;
+  let larguraPlayer = 500;
 
   const clicker = document.querySelector('#music-clicker');
   const seeker = document.querySelector('#music-seeker');
@@ -13,24 +17,20 @@ function mixagemAudioPlayer() {
   const playPauseBtn = document.querySelector('#playpause');
   const stopBtn = document.querySelector('#stop');
   const playlistMainTable = document.querySelector('#playlist-table');
-
-  const trackCount = 2;
-  const larguraPlayer = 300;                                                        // #####note to self#####: substituir por get.class - width value (depois remover o "px" )
-
+  const albumThumbnail = document.querySelector('#albumArtwork');
+  const artworkClick = document.querySelector('#artwork-mouseover')
 
   // função construtura - <td> título da faixa
-  function pTituloFaixa() {
+  function pTituloFaixa(i) {
     const pTituloFaixa = document.createElement('span');
-    // pTituloFaixa.innerText = `${megaArray[1][1]}`;
-    pTituloFaixa.innerText = `Nome da faixa oh yes`;
+    pTituloFaixa.innerText = `${megaArray[0][i]}`;
     return pTituloFaixa
   }
 
   // função construtura - <td> duração da faixa
-  function pDuraFaixa() {
+  function pDuraFaixa(i) {
     const pDuraFaixa = document.createElement('span');
-    // pDuraFaixa.innerText = `${tracklistTitulo[1]}`;
-    pDuraFaixa.innerText = `01:24`;
+    pDuraFaixa.innerText = `${megaArray[1][i]}`;
     return pDuraFaixa
   }
 
@@ -71,18 +71,30 @@ function mixagemAudioPlayer() {
   function playlistEntryInfo() {
     let i = 1;
     while (i <= trackCount) {
+
       playlistMainTable.appendChild(newTableRow());                                 // adiciona uma nova <tr>
       let linhaIndividual = playlistMainTable.lastChild;                            // seleciona a última <tr> introduzida
-      linhaIndividual.appendChild(pTituloFaixa());                                  // adiciona o <td> do título da faixa ao <tr>
-      linhaIndividual.appendChild(pDuraFaixa());                                    // adiciona o <td> da duração da faixa ao <tr>
+      linhaIndividual.appendChild(pTituloFaixa(i - 1));                                  // adiciona o <td> do título da faixa ao <tr>
+      linhaIndividual.appendChild(pDuraFaixa(i - 1));                                    // adiciona o <td> da duração da faixa ao <tr>
       let tituloFaixa = linhaIndividual.firstChild;                                 // seleciona o <td> do título da faixa
       tituloFaixa.classList.add('titulo-faixa', `faixa${i}`);                                    // adiciona a class ao <td> do título da faixa
       let duraFaixa = linhaIndividual.lastChild;                                    // seleciona o <td> da duração da faixa
       duraFaixa.classList.add('duracao-faixa', `faixa${i}`);                                     // adiciona a class ao <td> da duração da faixa
       i++;
     }
+    larguraPlayerOffset = playlistMainTable.offsetHeight * .7;                // *.7 serve para tornar a thumb mais quadrada
+    (function () {
+      document.querySelector('#music-clicker').style.width = `${larguraPlayer}px`;
+      document.querySelector('#music-seeker').style.width = `${larguraPlayer}px`;
+      document.querySelector('#playlist-table').style.width = `${larguraPlayer - larguraPlayerOffset}px`;
+    })()
   }
   playlistEntryInfo();
+
+  function albumArtwork() {
+    albumThumbnail.style.width = `${larguraPlayerOffset}px`;
+  }
+  albumArtwork();
 
   function autoplay() {
     audioID.onended = () => {
@@ -96,9 +108,9 @@ function mixagemAudioPlayer() {
 
   clicker.addEventListener('mousemove', mouseOver);                                 // função para atualizar a posição do seeker quando mouseoever
   clicker.addEventListener('click', mouseClick);                                    // função para atualizar a posição da faixa + progresso da faixa (.png)
-  playPauseBtn.addEventListener('click', () => { playpause(); progressBarTimer(); removeNowPlayingStyle(); nowPlayingStyle(); }); // função para o botão play/pause (play/pause + temporizador progress bar [se a música tiver parada, este fica em standby para poupar recursos])
+  playPauseBtn.addEventListener('click', () => { playpause(); playpauseArtwork(); progressBarTimer(); removeNowPlayingStyle(); nowPlayingStyle(); changeArtwork(); }); // função para o botão play/pause (play/pause + temporizador progress bar [se a música tiver parada, este fica em standby para poupar recursos])
   stopBtn.addEventListener('click', stop);
-
+  artworkClick.addEventListener('click', () => { playpause(); playpauseArtwork(); progressBarTimer(); removeNowPlayingStyle(); nowPlayingStyle(); changeArtwork(); });
 
   function getPosition(e) {                                                         // função para obter coordenada x do ponteiro do rato
     let rect = e.target.getBoundingClientRect();                                    // getBoundClienteRect é um objeto com as "geometrias" do objeto em relação à window
@@ -135,11 +147,11 @@ function mixagemAudioPlayer() {
 
   function stop() {
     try {
-      locales();
       audioID.pause();                                                                  // mete a música em pausa
       audioID.currentTime = 0;                                                          // retorna a música para o inicio (0s)
       clearInterval(timer);                                                           // limpa o temporizador existente
       progress.style.width = `0px`;                                                   // define a largura da progress bar como 0px
+      playpauseArtwork();
     } catch (err) { }
   }
 
@@ -147,14 +159,16 @@ function mixagemAudioPlayer() {
     let audioIDIndex = getAudioIDIndex();
     audioIDIndex++;
     if (audioIDIndex <= trackCount) {
-      audioID.src = `file:///D:/Documentos/Curso%20JavaScript/musicplayer/assets/mp3/batida${audioIDIndex}.mp3`;
+      audioID.src = `assets/mp3/audio${audioIDIndex}.mp3`;
       removeNowPlayingStyle();
       nowPlayingStyle();
+      changeArtwork(audioIDIndex);
       return audioID
     } else {
-      audioID.src = `file:///D:/Documentos/Curso%20JavaScript/musicplayer/assets/mp3/batida1.mp3`;
+      audioID.src = `assets/mp3/audio1.mp3`;
       removeNowPlayingStyle();
       nowPlayingStyle();
+      changeArtwork(1);
       return audioID
     }
   }
@@ -162,18 +176,16 @@ function mixagemAudioPlayer() {
 
   function trackSelect(e) {
     let gridTarget = e.target;
-    console.log(gridTarget);
     let gridClassName = gridTarget.className;
-    console.log(gridClassName);
     let gridClassNameIndex = gridClassName[gridClassName.length - 1]
-    console.log(gridClassNameIndex);
-    let gridTrackIndex = gridClassNameIndex;
-    audioID.src = `file:///D:/Documentos/Curso%20JavaScript/musicplayer/assets/mp3/batida${gridTrackIndex}.mp3`;
+    audioID.src = `assets/mp3/audio${gridClassNameIndex}.mp3`;
     removeNowPlayingStyle();
     nowPlayingStyle();
     stop();
     playpause();
+    playpauseArtwork();
     progressBarTimer();
+    changeArtwork(gridClassNameIndex);
   }
 
   const gay1 = document.querySelector('.titulo-faixa' + '.faixa1');
@@ -199,32 +211,26 @@ function mixagemAudioPlayer() {
     } else return
   });
 
-  function locales() {
 
-    fetch('assets/js/tracklist.json')
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        appendData(data);
-      })
-      .catch(function (err) {
-        console.log('error: ' + err);
-      });
+  function playpauseArtwork() {
+    if (!audioID.paused) {
+      artworkClick.innerHTML = 'ON';
+    }                                              // se tiver em pausa, mete play
+    else {
+      artworkClick.innerHTML = 'OFF';
+    };                                                        // se tiver em play, mete pausa
+  }
 
-    function appendData(data) {
-      for (let i = 1; i <= data.length; i++) {
-        let track = document.querySelector(`.titulo-faixa`+`.faixa${i}`).innerText = data[i-1].trackName;
-        console.log(track);
-        let duration = document.querySelector(`.duracao-faixa`+`.faixa${i}`).innerText = data[i-1].trackLenght;
-        console.log(duration);
-      }
+  function changeArtwork(i) {
+    if (i === undefined) { 
+      let indexForThumb = getAudioIDIndex();
+      albumThumbnail.style.backgroundImage = `url('assets/img/thumb${indexForThumb}.jpg')`;
     }
-    
-
-  } 
-
-  
+    else {
+      albumThumbnail.style.backgroundImage = `url('assets/img/thumb${i}.jpg')`;
+    }
+  }
 
 }
+
 mixagemAudioPlayer();
